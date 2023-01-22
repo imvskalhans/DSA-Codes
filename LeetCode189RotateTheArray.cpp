e71e65b2e7b96a34dd938d7c727e92342798f30b
imvskalhans <vsk6645@bbdu.ac.in>
2023-01-22 19:59:34 +0530

    rotate array commit

--- LeetCode189RotateTheArray.cpp
+++ LeetCode189RotateTheArray.cpp
@@ -1,7 +1,14 @@
+/*
+22 Jan solution by Vishal Singh
+Leetcode
+189. Rotate Array
+Medium
+Given an integer array nums,
+rotate the array to the right by k steps, where k is non-negative.
 #include<iostream>
 using namespace std;
-
-/*int rotate(int arr[],int s,int k)
+//rotating right
+int rotate(int arr[],int s,int k)
 {
    int j=0;
     while(j<k)
@@ -24,6 +31,9 @@ using namespace std;
     
 }
 */
+
+// rotating left
+/*
 int rotate(int arr[],int s,int k)
 {
    int j=0;
@@ -46,15 +56,59 @@ int rotate(int arr[],int s,int k)
     }
     
 }
+*/
+// leetcode approach by lbDSA
+
+    /*void rotate(vector<int>& nums, int k) {
+        vector<int> tes(nums);
+        int j=0;
+        int s=nums.size();
+    while(j<k)
+    {
+       int temp=tes[s-1];
+       int i=s;
+        for(;i>0;i--)
+        {
+            tes[i]=tes[i-1];
+        }
+        tes[i]=temp;
+        j++;
+
+    }   
+    nums=tes;     
+    }
+*/
+    
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> rotate(vector<int>& nums, int k)
+{
+    vector<int> temp(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            temp[(i+k)%nums.size()]=nums[i];
+        }
+            nums=temp;
+            return nums;
+}
+
+
 
 int main()
 {
     int k;
-    cout<<"k =";
+    cout<<"enter k";
     cin>>k;
-   
-    int arr[100]={1,2,3,4,5,6,7};
-    int search =rotate(arr,7,k);
-  
+    vector<int> nums;
+    cout<<"enter nums"<<endl;
+     int input;
+    while (std::cin >> input) {
+        nums.push_back(input);
+    }
+    vector<int> result = rotate(nums,k); 
+for (int i : result) {
+    cout << i << " ";
 }
-
+   
+}
