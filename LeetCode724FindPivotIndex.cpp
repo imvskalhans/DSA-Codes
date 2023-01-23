a8ee18c9d9d8f521c0c1126741e1970dbd676f83
imvskalhans <vsk6645@bbdu.ac.in>
2023-01-23 23:54:48 +0530

    Leetcode724 commit

--- LeetCode724FindPivotIndex.cpp
+++ LeetCode724FindPivotIndex.cpp
@@ -1,50 +1,83 @@
+/*
+23 Jan solution by Vishal Singh
+Leetcode
+724. Find Pivot Index
+Easy
+Given an array of integers nums, calculate the pivot index of this array.
+The pivot index is the index where the sum of all the numbers strictly to the left of the index 
+is equal to the sum of all the numbers strictly to the index's right.
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements
+to the left. This also applies to the right edge of the array.
+Return the leftmost pivot index. If no such index exists, return -1.
+
+//best approach of solution
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total_sum = accumulate(nums.begin(), nums.end(), 0); 
+        int left_sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (left_sum == total_sum - left_sum - nums[i]) {
+                return i;
+            }
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+};
+
+
+
+
+*/
+
+
 #include<iostream>
+#include<vector>
 using namespace std;
-int pivot(int arr[],int size)
-{
-    int first=0,last=size-1;
-    int ls=0,rs=0;
-     int mid=first + (last-first/2);
-     int sumls(int mid){
-     for(int i=0;i<mid;i++)
-     {
-        ls=ls+arr[i];
-     }
-     return ls;
-     }
-     int sumrs(int mid){
-     for(int i=mid+1;i<=last;i++)
-     {
-        rs=rs+arr[i];
-     }
-     return rs;
-     }
-    while(first<=last)
-    {
-      int pivotele(int rs,int ls){
-        if(ls==rs)
-        {
-            cout<<"piviot found"<<mid<<endl;  
-        }
-         else if(rs>ls)
-         {
-                mid=mid+1;
+
+//this approach of mine still need some correction
+ int pivotIndex(vector<int>& nums) {
+        int start = 0, end = nums.size()-1;
+        int mid = start + (end-start)/2;
+        while(start<=end)
+        { 
+           // int mid = start + (end-start)/2;
+            long int sum1 = 0;
+            for(int i=0;i<mid;i++)
+            {
+                sum1=sum1+nums[i];
+            }
+            long int sum2 = 0;
+            for(int i=mid+1;i<=end;i++)
+            {
+                sum2=sum2+nums[i];
+            }
+            if(sum1 == sum2)
+            {
                 return mid;
-         }
-         else{
-            mid=mid-1;
-            return mid;
-         }
-      }
-        
+            }
+            else if(sum1 < sum2)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+            mid = start + (end-start)/2;
         }
-    
-}
-int main()
+        return -1;
+    } 
+
+    int main()
 {
-    int size;
-    int arr[100]={1,7,3,6,5,6};
-    int search =pivot(arr,6);
-   cout<<"peak value =" <<pivot<<endl;
-    
-} 
+    vector<int> nums;
+    cout<<"enter nums"<<endl;
+     int input;
+    while (std::cin >> input) {
+        nums.push_back(input);
+    }
+    double result=pivotIndex(nums);
+    cout<<"pivot index = "<<result<<endl;
+}
