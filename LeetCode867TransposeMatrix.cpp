90a0c9b3afe629de7766ada31019388951652cc7
imvskalhans <vsk6645@bbdu.ac.in>
2023-01-26 01:10:06 +0530

    leetcode 867

---
+++ LeetCode867TransposeMatrix.cpp
@@ -0,0 +1,59 @@
+/*
+LeetCode
+867. Transpose Matrix
+Easy
+Given a 2D integer array matrix, return the transpose of matrix.
+The transpose of a matrix is the matrix flipped over its main diagonal,
+ switching the matrix's row and column indices.
+
+
+*/
+#include<iostream>
+ #include<vector>
+ #include<algorithm>
+ #include <iomanip>
+using namespace std;
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+
+vector<vector<int>> ans (matrix[0].size(), vector<int>(matrix.size()));
+         for(int i=0;i<matrix.size();i++)
+		 {
+			 for(int j=0;j<matrix[0].size();j++)
+			 {
+				 ans[j][i]=matrix[i][j];
+			 }
+		 }
+         return ans;
+    }
+
+int main() {
+    int nRows, mCols;
+    cout<<"enter no of rows and columns"<<endl;
+    cin >> nRows >> mCols;
+    cout<<"enter the matrix entries"<<endl;
+    vector<vector<int>> my2dvector(nRows, vector<int>(mCols));
+
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < mCols; j++) {
+            cin >> my2dvector[i][j];
+        }
+    }
+
+    cout<<"entered matrix is - "<<endl;
+    
+    for (int i = 0; i < my2dvector.size(); i++) {
+        for (int j = 0; j < my2dvector[i].size(); j++) {
+            std::cout << my2dvector[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    vector<vector<int>> result = transpose(my2dvector);
+    cout<<"transpose of matrix "<<endl;
+    for(int i=0;i<result.size();i++)
+    {
+        for(int j=0;j<result[0].size();j++){
+        cout<<result[i][j]<<" ";
+    }cout<<endl;
+}
+}
