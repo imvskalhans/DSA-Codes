0b79c600249280535116f4d47240e38497676892
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-04 21:06:37 +0530

    1752

--- LeetCode1752.cpp
+++ LeetCode1752.cpp
@@ -1,3 +1,13 @@
+/*
+1752. Check if Array Is Sorted and Rotated
+Easy 
+Given an array nums, return true if the array was originally sorted in 
+non-decreasing order, then rotated some number of positions (including zero). 
+Otherwise, return false.
+There may be duplicates in the original array.
+Note: An array A rotated by x positions results in an array B of the same 
+length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
+*/
 #include<bits/stdc++.h> 
 #include<iostream>
 using namespace std;
