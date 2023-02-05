01be3bea163688ca5e0e653091aecd01879835f7
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-05 18:45:53 +0530

    442

--- LeetCode442FindAllDuplicates.cpp
+++ LeetCode442FindAllDuplicates.cpp
@@ -6,6 +6,19 @@ are in the range [1, n] and each integer appears once or twice, return
 an array of all the integers that appears twice.
 You must write an algorithm that runs in O(n) time and uses only constant extra space.
 
+//best approach
+  vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;  // 1. Calculate the index based on the current value in nums
+            if (nums[index] < 0) {  // 2. Check if the number at this index has already been marked as a duplicate
+                ans.push_back(index + 1);  // 3. If yes, add the number to the ans vector
+            } else {  // 4. If not, mark the number at this index as a duplicate
+                nums[index] = -nums[index];
+            }
+        }
+        return ans;  // 5. Return the ans vector, which contains the duplicates
+    }
  
 */
 #include<iostream>
