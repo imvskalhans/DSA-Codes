b0c5cf98d8b595b15103e47ca619f33bbb6d9aae
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-12 19:55:39 +0530

    LC 2180

---
+++ LeetCode2180CountIntegersWithEvenDigitSum.cpp
@@ -0,0 +1,26 @@
+/*
+Leetcode 2180
+*/
+ #include<iostream>
+ using namespace std;
+
+int countEven(int num) {
+int count = 0;
+    for (int i = 1; i <= num; i++) {
+        int digitSum = 0;
+        int n = i;
+        while (n > 0) {
+            digitSum += n % 10;
+            n /= 10;
+        }
+        if (digitSum % 2 == 0) count++;
+    }
+    return count;
+}
+int main()
+ {
+    int num;
+    cout<<"enter the value of n"<<endl;
+    cin>>num;
+    cout<< "pivotInteger ="<<countEven(num)<<endl;
+ }
