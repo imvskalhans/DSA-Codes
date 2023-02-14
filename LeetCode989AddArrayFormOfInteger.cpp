184f7bc9234a273e0d7e99116998c75b0ac9a455
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-15 01:04:18 +0530

    lc 989

---
+++ LeetCode989AddArrayFormOfInteger.cpp
@@ -0,0 +1,49 @@
+/*
+lc 989
+
+*/
+//will fail for 999999 + 1 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ vector<int> addToArrayForm(vector<int>& num, int k) {
+        int number=0;
+        int sum=0;
+        vector<int> ans;
+        for(int i=0;i<num.size();i++)
+        {
+            number=number*10 + num[i];
+        }
+        cout<<"number"<<number<<endl;
+        sum=number+k;
+       // cout<<"sum 1"<<sum<<endl;
+        while(sum>0)
+        {
+            int d=sum%10;
+            //cout<<"sum 2"<<sum<<endl;
+            ans.insert(ans.begin(),d);
+
+            sum=sum/10;
+           // cout<<"sum 3"<<sum<<endl;
+        }
+        return ans;
+    }
+
+int main()
+{
+    int k;
+    cout<<"enter k "<<endl;
+    cin>>k;
+    vector<int> nums;
+    cout<<"enter nums"<<endl;
+     int input;
+    while (std::cin >> input) {
+        nums.push_back(input);
+    }
+    vector<int> result=addToArrayForm(nums,k);
+    for(int i=0;i<result.size();i++)
+   {
+    cout<<result[i]<<" ";
+   }
+}
