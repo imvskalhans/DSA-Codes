cd57dd4d191dfe360e48c4f13fcd253d34c6eed6
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-15 21:05:40 +0530

    Lc 2161

---
+++ LeetCode2161PartitionArrayAccToPivot.cpp
@@ -0,0 +1,46 @@
+/*
+
+*/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+vector<int> pivotArray(vector<int>& nums, int pivot) {
+       // sort(nums.begin(), nums.end());
+        int i=0;
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]<pivot)
+            {
+            }
+            else
+            {
+               nums.insert(nums.begin() + i, pivot);
+               nums.erase(nums.begin() + i);
+
+               break;
+            }
+        }
+       // nums.pop_back();
+        //sort(nums.begin(), nums.end() - (i+1), greater<int>());
+        return nums;
+        
+    }
+int main()
+{
+    int k;
+    cout<<"enter k"<<endl;
+    cin>>k;
+
+    vector<int> nums;
+    cout<<"enter nums"<<endl;
+     int input;
+    while (std::cin >> input) {
+        nums.push_back(input);
+    }
+    vector<int> result=pivotArray(nums,k);
+    for(int i=0; i< nums.size();i++){
+        cout<<" "<<result[i]<<" ";
+    }
+    
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,18 @@
-cout <<s<<endl;
+ int i=0;
+        for(i=0;i<nums.size();i++)
+        {
+            if(nums[i]<pivot)
+            {
+            }
+            else
+            {
+               nums.insert(nums.begin() + i, pivot);
+               nums.erase(nums.begin() + i);
+
+               break;
+            }
+        }
+       // nums.pop_back();
+        //sort(nums.begin(), nums.end() - (i+1), greater<int>());
+        return nums;
+        
