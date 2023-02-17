71b21127b51459a1fd59452a4d6446d652b46a5c
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-17 20:22:04 +0530

    387

---
+++ LeetCode387FindUnique.cpp
@@ -0,0 +1,53 @@
+/*
+
+*/
+
+#include<iostream>
+using namespace std;
+//approach one
+int firstUniqChar(string s)
+{
+    int l=s.size()-1;
+    int i=0,j=0;
+    for(i=0;i<=l;i++)
+    {
+        for(j=0;j<=l;j++)
+        {
+            if(i!=j&&s[i]==s[j])
+            {
+                break;
+            }
+        }
+        if(j=l+1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+// approach two best one
+int firstUniqChar2(string s)
+{
+    int l=s.size()-1;
+    int ch[26]={0};
+    for(auto &c : s)
+    {
+        ch[(c-'a')]++;
+    }
+    for(int i=0;i<=l;i++)
+    {
+        
+    }
+}
+
+
+int main()
+{
+    cout<<"enter the string"<<endl;
+    string command;
+    getline(cin,command);
+    int result=firstUniqChar(command);
+    cout<<"index of first unique char "<<result<<endl;
+    int result2=firstUniqChar2(command);
+    cout<<"index of first unique char "<<result2<<endl;
+}
--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,3 +1,96 @@
 /*
 
 */
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int> &num, int s, int e)
+{
+    //creating  two arrays
+    int mid=s+(e-s)/2;
+    int len1=mid-s+1;
+    int len2=e-s;
+    
+    vector<int> first (len1);
+    
+    vector<int> second (len2);
+
+
+    //copying values in two arrays
+    int mainarrayindex = s;
+    for(int i=0;i<len1;i++)
+    {
+        first[i]=num[mainarrayindex++];
+    }
+     for(int i=0;i<len2;i++)
+    {
+        second[i]=num[mainarrayindex++];
+    }
+    
+    //merge two sorted arrays
+    int index1=0;
+    int index2=0;
+    mainarrayindex = s;
+    while(index1<len1&&index2<len2)
+    {
+        if(first[index1]<second[index2])
+        {
+            num[mainarrayindex++]=first[index1];
+        }
+        else{
+            num[mainarrayindex++]=second[index2];
+        }
+
+    }
+    while(index1<len1)
+    {
+        num[mainarrayindex++]=first[index1];
+    }
+    while(index2<len2)
+    {
+        num[mainarrayindex++]=second[index2];
+    }
+
+
+}
+
+void mergesort(vector<int> &num , int s , int e )
+{
+    //base case
+    if(s>=e)
+    {
+        return;
+    }
+    int mid=s+(e-s)/2;
+    //left part breaking
+    mergesort(num,s,mid);
+
+    //right part breaking
+    mergesort(num,mid+1,e);
+
+    //merging and sorting two arrays
+    merge(num,s,e);
+
+
+
+
+}
+
+
+int main()
+{
+    vector<int> num;
+    cout<<"enter num"<<endl;
+     int input;
+    while (std::cin >> input) {
+        num.push_back(input);
+    }
+    int s=0;
+    int e=num.size()-1;
+    mergesort(num,s,e);
+    for(int i=0; i< num.size();i++){
+        cout<<" "<<num[i]<<" ";
+    }cout<<endl;
+    
+}
