f6193425cff0c516387f4a1be2ef41cb5c26b2f8
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-19 21:29:38 +0530

    78 subsets

--- LeetCode78Subsets.cpp
+++ LeetCode78Subsets.cpp
@@ -23,4 +23,51 @@ private:
 };
 
 */
+#include<iostream>
+#include<vector>
+using namespace std;
 
+
+void solve(vector<int> nums, int index , vector<int> output , vector<vector<int>>& ans)
+    {
+        //base case
+        if(index>=nums.size())
+        {
+            ans.push_back(output);
+            return;
+        }
+
+        //exclude call
+        solve(nums,index+1,output, ans);
+
+        //include call
+        output.push_back(nums[index]);
+        solve(nums,index+1,output, ans);
+    }
+
+vector<vector<int>> subsets(vector<int>& nums) {
+         vector<vector<int>> ans;
+         vector<int> output;
+         int index=0;
+         solve(nums,index,output, ans);
+         return ans;
+    }
+
+int main()
+{
+    vector<int> nums;
+    cout<<"enter nums"<<endl;
+     int input;
+    while (std::cin >> input) {
+        nums.push_back(input);
+    }
+    vector<vector<int>> result=subsets(nums);
+    cout<<"power set  = ";
+    for(int i=0;i<result.size();i++)
+    {
+        for(int j=0;j<result[0].size();j++){
+           cout<<result[i][j]<<" "; 
+        }cout<<endl;
+        
+    }
+}
