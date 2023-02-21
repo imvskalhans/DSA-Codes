892131aea7c73dd4166917aa5f0e31eeaf57438a
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-21 11:58:26 +0530

    print1ton

--- StrecPrint1toN.cpp
+++ StrecPrint1toN.cpp
@@ -1,2 +1,25 @@
 #include<iostream>
 using namespace std;
+void printn( int c, int n)
+{
+    //base case
+    if(c==n)
+    {
+        return;
+    }
+    //recursive call
+    cout<<" "<<c<<" ";
+   // c=c+1;
+    //c++;
+    printn(++c,n);
+    
+}
+
+int main()
+{
+    int n;//cout<<"Enter";
+    cin>>n;
+    int c=1;
+    
+    printn(c,n);
+}
