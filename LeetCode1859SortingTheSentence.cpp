f8a4f157033437156d1343739cd00c7420318a33
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-23 21:06:35 +0530

    1859

---
+++ LeetCode1859SortingTheSentence.cpp
@@ -0,0 +1,49 @@
+/*
+
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string sortSentence(string s)
+{
+    vector<string> words; // Create a vector to store the words in the shuffled sentence
+    string word = "";
+    for (char c : s)
+    {
+        if (c == ' ')
+        {
+            words.push_back(word);
+            word = "";
+        }
+        else
+        {
+            word += c;
+        }
+    }
+    words.push_back(word);
+    string result = "";
+    for (int i = 1; i <= words.size(); i++)
+    {
+        for (string w : words)
+        {
+            if (w[w.size() - 1] == i + '0')
+            {
+                result += w.substr(0, w.size() - 1) + " ";
+            }
+        }
+    }
+    return result.substr(0, result.size() - 1);
+    
+}
+
+
+int main()
+{
+    string s;
+    cout << "enter the string s" << endl;
+    getline(cin, s);
+    string result = sortSentence(s);
+    cout << result;
+}
