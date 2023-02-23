3e4d18cc30d01b7fa0c58e70f1b46146ed41ece9
imvskalhans <vsk6645@bbdu.ac.in>
2023-02-23 21:10:37 +0530

    documentation

--- LeetCode1859SortingTheSentence.cpp
+++ LeetCode1859SortingTheSentence.cpp
@@ -1,11 +1,17 @@
 /*
+1859. Sorting the Sentence
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
 
 */
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
-
+//
+// Created by Vishal on 2021/8/3.
 string sortSentence(string s)
 {
     vector<string> words; 
@@ -38,7 +44,7 @@ string sortSentence(string s)
 
 }
 
-
+// Driver Code
 int main()
 {
     string s;
