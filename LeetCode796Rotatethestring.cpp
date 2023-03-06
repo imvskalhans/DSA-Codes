279e689fa4b0865fe53a6ea6f9e8e788a389151b
imvskalhans <vsk6645@bbdu.ac.in>
2023-03-07 00:12:51 +0530

    796

--- LeetCode796Rotatethestring.cpp
+++ LeetCode796Rotatethestring.cpp
@@ -9,40 +9,8 @@ Easy
 using namespace std;
 
 bool rotateString(string A, string B) {
-    if(A.size()!=B.size())
-    {
-        return false;
-    }
-    if(A==B)
-    {
-        return true;
-    }
-    int i=0;
-    int j=0;
-    int k=0;
-    while(i<A.size())
-    {
-        if(A[i]==B[j])
-        {
-            i++;
-            j++;
-        }
-        else
-        {
-            i=0;
-            j=0;
-            k++;
-            j=k;
-        }
-    }
-    if(i==A.size())
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+  
+
 }
 
  int main()
