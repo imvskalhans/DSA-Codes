c6b4f6e7e6588cbcd3b76d62c6a7fc88813e9f53
imvskalhans <vsk6645@bbdu.ac.in>
2023-03-08 00:11:30 +0530

    754

--- LeetCode754ReachNumber.cpp
+++ LeetCode754ReachNumber.cpp
@@ -5,3 +5,48 @@ Medium
 */
 
 #include<iostream>
+#include<vector>
+using namespace std;
+
+int reachNumber(int target)
+{
+    int i = 1;
+    int sum = 0;
+    while (sum < target)
+    {
+        sum = sum + i;
+        i++;
+    }
+    if (sum == target)
+    {
+        return i - 1;
+    }
+    else
+    {
+        int diff = sum - target;
+        if (diff % 2 == 0)
+        {
+            return i - 1;
+        }
+        else
+        {
+            if (i % 2 == 0)
+            {
+                return i;
+            }
+            else
+            {
+                return i + 1;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int target;
+    cout << "Enter the target: ";
+    cin >> target;
+    cout << reachNumber(target);
+    return 0;
+}
