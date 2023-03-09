f1b93bb27975341052dd91c453f3324f09bebdf4
imvskalhans <vsk6645@bbdu.ac.in>
2023-03-09 17:48:23 +0530

    141

---
+++ LeetCode141LinkedlistCycle.cpp
@@ -0,0 +1,44 @@
+/*
+leetcode 141 Linked List Cycle
+Given a linked list, determine if it has a cycle in it.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head) {
+    if (head == NULL || head->next == NULL) {
+        return false;
+    }
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while (slow != fast) {
+        if (fast == NULL || fast->next == NULL) {
+            return false;
+        }
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return true;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    ListNode* node1 = new ListNode(2);
+    ListNode* node2 = new ListNode(3);
+    ListNode* node3 = new ListNode(4);
+    ListNode* node4 = new ListNode(5);
+    head->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node2;
+    cout << hasCycle(head) << endl;
+}
