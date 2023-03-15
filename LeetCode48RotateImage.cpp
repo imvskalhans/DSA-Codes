4ad07fd41f0f0167727bef17fcc5830ad71d34c4
imvskalhans <vsk6645@bbdu.ac.in>
2023-03-15 23:17:28 +0530

    48

--- LeetCode48RotateImage.cpp
+++ LeetCode48RotateImage.cpp
@@ -49,3 +49,44 @@ int main() {
     }cout<<endl;
 }
 }
+
+/*
+approach 1: transpose and reverse
+approach 2: rotate 4 times
+code 1:
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+code 2:
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        for(int i=0;i<n/2;i++)
+        {
+            for(int j=i;j<n-i-1;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[n-j-1][i];
+                matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
+                matrix[j][n-i-1]=temp;
+            }
+        }
+    }
+};
+*/
