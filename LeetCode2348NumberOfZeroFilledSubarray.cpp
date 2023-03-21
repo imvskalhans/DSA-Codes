443f15b18a7f7150069ba3f854ba5486496d023a
imvskalhans <vsk6645@gmail.com>
2023-03-21 08:57:10 +0530

    2348

--- LeetCode2348NumberOfZeroFilledSubarray.cpp
+++ LeetCode2348NumberOfZeroFilledSubarray.cpp
@@ -36,3 +36,30 @@ int main()
     cout << numberOfZeroFilledSubarray(nums) << endl;
     return 0;
 }
+
+/*
+approach 2:
+int numberOfZeroFilledSubarray(vector<int>& nums)
+{
+    int count = 0;
+    int n = nums.size();
+    int i = 0;
+    while (i < n)
+    {
+        if (nums[i] == 0)
+        {
+            int j = i + 1;
+            while (j < n && nums[j] == 0)
+                j++;
+            count += (j - i) * (j - i + 1) / 2;
+            i = j;
+        }
+        else
+            i++;
+    }
+    return count;
+}
+
+approach 3
+
+*/
