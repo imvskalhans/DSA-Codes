2c164ec1c49d258631ecc2e7a265ae7601a96b4a
imvskalhans <vsk6645@gmail.com>
2023-03-22 17:32:26 +0530

    735

--- LeetCode735AsteroidsCollision.cpp
+++ LeetCode735AsteroidsCollision.cpp
@@ -16,5 +16,52 @@ Two asteroids moving in the same direction will never meet.
 #include <vector>
 #include <unordered_map>
 #include <stack>
+#include <algorithm>
 using namespace std;
 
+vector<int> asteroidCollision(vector<int>& asteroids)
+{
+    stack<int> s;
+    for (int i = 0; i < asteroids.size(); i++)
+    {
+        if (asteroids[i] > 0)
+        {
+            s.push(asteroids[i]);
+        }
+        else
+        {
+            while (!s.empty() && s.top() > 0 && s.top() < -asteroids[i])
+            {
+                s.pop();
+            }
+            if (s.empty() || s.top() < 0)
+            {
+                s.push(asteroids[i]);
+            }
+            else if (s.top() == -asteroids[i])
+            {
+                s.pop();
+            }
+        }
+    }
+    vector<int> result;
+    while (!s.empty())
+    {
+        result.push_back(s.top());
+        s.pop();
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+int main()
+{
+    vector<int> asteroids = { 5, 10, -5 };
+    vector<int> result = asteroidCollision(asteroids);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
