d06b1efcb95a05a0e426ebec071167ae44b3176e
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-05-23 17:47:54 +0530

    Time: 212 ms (8.28%), Space: 96.3 MB (85.19%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    bool isMonotonic(vector<int>& nums) {
+        //optimized solution
+        bool increasing = true;
+        bool decreasing = true;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                increasing = false;  // Found a decreasing pair
+            }
+            if (nums[i] < nums[i + 1]) {
+                decreasing = false;  // Found an increasing pair
+            }
+        }
+
+        return increasing || decreasing;
+    }
+};
+
+
+/*
+//my solution
+class Solution {
+public:
     bool increasing(vector<int>& nums) {
         for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i] > nums[i + 1]) {
@@ -22,3 +45,4 @@ public:
         return increasing(nums) || decreasing(nums);
     }
 };
+*/
