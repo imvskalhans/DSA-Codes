498b16f3c679a75f6a5eadef93625911e7959acb
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-05-26 11:48:21 +0530

    Time: 8 ms (48.21%), Space: 6 MB (8.8%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,3 +1,30 @@
+//optimized using binary search
+class Solution {
+public:
+    int arrangeCoins(int n) 
+    {
+       int low =1;
+       int high = n;
+       int mid =0;
+       while(low<=high){
+         long mid = low+(high-low)/2;
+           long  total = (long) (mid*(mid+1))/2;
+           if(total==n){
+               return mid;
+           }
+           else if(total<n){
+               low=mid+1;
+           }
+           else{
+               high = mid-1;
+           }
+       }
+       return high;
+    }
+};
+
+//my solution
+/*
 class Solution {
 public:
     int arrangeCoins(int n) {
@@ -10,3 +37,4 @@ public:
         return stairs-2;
     }
 };
+*/
