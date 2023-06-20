5de3e7db4f650d79a9db7b744ee6317fa89c77e0
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-06-21 01:49:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse Coding/reverse-coding.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        int mod = 1e9+7;
+        return (((long long)n*(n+1)%mod)/2)%mod;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
