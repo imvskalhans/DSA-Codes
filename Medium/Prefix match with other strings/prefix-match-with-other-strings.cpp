9b3a886369463be79cd904fecb47779893ff8ef0
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-06-24 23:16:36 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prefix match with other strings/prefix-match-with-other-strings.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int klengthpref(string arr[], int n, int k, string str){    
+              if(k>str.length())return 0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(k<=arr[i].length()){
+                int j=0;
+                while(j<k){
+                    if(arr[i][j]!=str[j])break;
+                    j++;
+                }
+                if(j==k)ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string arr[n];
+        for(int i=0;i<n;i++)
+        {
+            string s;
+            cin>>arr[i];
+            
+        }
+        int k;
+        cin>>k;
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout << ob.klengthpref(arr, n, k, str) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
