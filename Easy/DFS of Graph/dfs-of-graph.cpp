1c8011c479b9642572ee33a5cec539237c43b28d
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-08-01 19:06:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  // Helper function for DFS traversal from a given node
+void dfsUtil(int node, vector<int> adj[], vector<bool>& visited, vector<int>& result) {
+    // Mark the current node as visited
+    visited[node] = true;
+    // Add the current node to the result list
+    result.push_back(node);
+
+    // Visit all the adjacent nodes that are not yet visited
+    for (int neighbor : adj[node]) {
+        if (!visited[neighbor]) {
+            dfsUtil(neighbor, adj, visited, result);
+        }
+    }
+}
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+            vector<bool> visited(V, false); // Initialize all nodes as not visited
+    vector<int> result; // To store the DFS traversal result
+
+    // Call the helper function for all unvisited nodes in the graph
+    for (int i = 0; i < V; ++i) {
+        if (!visited[i]) {
+            dfsUtil(i, adj, visited, result);
+        }
+    }
+
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
