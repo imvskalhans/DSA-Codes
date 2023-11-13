57936c07c0915c8a74d0ab2629fd86da9a4db993
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-11-13 10:00:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to find length of shortest common supersequence of two strings.
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        //code here
+            // Create a 2D array to store the lengths of the common supersequences.
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Fill the dp array using bottom-up dynamic programming.
+    for (int i = 0; i <= m; ++i) {
+        for (int j = 0; j <= n; ++j) {
+            if (i == 0 || j == 0) {
+                dp[i][j] = i + j;
+            } else if (X[i - 1] == Y[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // The length of the shortest common supersequence is stored in the bottom-right cell.
+    return dp[m][n];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{   
+    
+    int t;
+    
+    //taking total testcases
+    cin >> t;
+    while(t--){
+    string X, Y;
+    //taking String X and Y
+	cin >> X >> Y;
+	
+	//calling function shortestCommonSupersequence()
+	Solution obj;
+	cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size())<< endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
