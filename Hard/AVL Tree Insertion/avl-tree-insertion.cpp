9dde46884d70d141307495be8400908d4795bff7
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-11-23 09:16:24 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/AVL Tree Insertion/avl-tree-insertion.cpp
@@ -0,0 +1,210 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+#define MAXN 2000
+using namespace std;
+
+struct Node
+{
+    int data, height;
+    Node *left, *right;
+    
+    Node(int x)
+    {
+        data=x;
+        left=right=NULL;
+        height=1;
+    }
+};
+
+bool isBST(Node *n, int lower, int upper)
+{
+	if(!n) return 1;
+	if( n->data <= lower || n->data >= upper ) return 0;
+	return isBST(n->left, lower, n->data) && isBST(n->right, n->data, upper) ;
+}
+
+pair<int,bool> isBalanced(Node* n)
+{
+	if(!n) return pair<int,bool> (0,1);
+
+	pair<int,bool> l = isBalanced(n->left);
+	pair<int,bool> r = isBalanced(n->right);
+
+	if( abs(l.first - r.first) > 1 ) return pair<int,bool> (0,0);
+
+	return pair<int,bool> ( 1 + max(l.first , r.first) , l.second && r.second );
+}
+
+bool isBalancedBST(Node* root)
+{
+	if( !isBST(root, INT_MIN, INT_MAX) )
+		cout<< "BST voilated, inorder traversal : ";
+
+	else if ( ! isBalanced(root).second )
+		cout<< "Unbalanced BST, inorder traversal : ";
+
+	else return 1;
+	return 0;
+}
+
+void printInorder(Node* n)
+{
+	if(!n) return;
+	printInorder(n->left);
+	cout<< n->data << " ";
+	printInorder(n->right);
+}
+
+
+// } Driver Code Ends
+/* The structure of the Node is
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+    int height;
+};
+*/
+
+class Solution{
+  public:
+// Function to get the height of the node
+int getHeight(Node *node)
+{
+    if (node == NULL)
+        return 0;
+    return node->height;
+}
+
+// Function to get the balance factor of the node
+int getBalance(Node *node)
+{
+    if (node == NULL)
+        return 0;
+    return getHeight(node->left) - getHeight(node->right);
+}
+
+// Function to perform right rotation
+Node *rightRotate(Node *y)
+{
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    return x;
+}
+
+// Function to perform left rotation
+Node *leftRotate(Node *x)
+{
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+    return y;
+}
+
+// Function to insert a node into the AVL tree
+Node *insertToAVL(Node *root, int data)
+{
+    // Step 1: Perform the normal BST insertion
+    if (root == NULL)
+        return new Node(data);
+
+    if (data < root->data)
+        root->left = insertToAVL(root->left, data);
+    else if (data > root->data)
+        root->right = insertToAVL(root->right, data);
+    else // Duplicate data not allowed
+        return root;
+
+    // Step 2: Update height of the current node
+    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+
+    // Step 3: Get the balance factor of this ancestor node
+    int balance = getBalance(root);
+
+    // Step 4: If the node is unbalanced, perform rotations
+
+    // Left Left Case
+    if (balance > 1 && data < root->left->data)
+        return rightRotate(root);
+
+    // Right Right Case
+    if (balance < -1 && data > root->right->data)
+        return leftRotate(root);
+
+    // Left Right Case
+    if (balance > 1 && data > root->left->data)
+    {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // Right Left Case
+    if (balance < -1 && data < root->right->data)
+    {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
+// Function to perform inorder traversal of the AVL tree
+void inorder(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int ip[MAXN];
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        for(int i=0; i<n; i++)
+            cin>> ip[i];
+        
+        Node* root = NULL;
+        Solution obj;
+        for(int i=0; i<n; i++)
+        {
+            root = obj.insertToAVL( root, ip[i] );
+            
+            if ( ! isBalancedBST(root) )
+                break;
+        }
+        
+        printInorder(root);
+        cout<< endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
