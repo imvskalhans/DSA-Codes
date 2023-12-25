95021c52da954ffad4266c9408f5ded5544ecbe4
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2023-12-25 20:42:50 +0530

    Updated Solution - GfG to GitHub

--- Easy/Determinant of a Matrix/determinant-of-a-matrix.cpp
+++ Easy/Determinant of a Matrix/determinant-of-a-matrix.cpp
@@ -9,42 +9,34 @@ class Solution
 {   
     public:
     //Function for finding determinant of matrix.
-    int determinantOfMatrix(vector<vector<int> > matrix, int n)
+ int determinantOfMatrix(vector<vector<int>> &matrix, int n)
     {
-        // code here 
-             // Base case: if the matrix is 1x1, return its only element
-        if (n == 1) {
+        if(n == 1)
             return matrix[0][0];
-        }
-
-        int det = 0;
-
-        // Iterate over the first row elements
-        for (int i = 0; i < n; i++) {
-            // Calculate the sign multiplier
-            int sign = (i % 2 == 0) ? 1 : -1;
-
-            // Calculate the minor matrix
-            vector<vector<int>> minorMatrix(n - 1, vector<int>(n - 1));
-            for (int j = 1; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    if (k < i) {
-                        minorMatrix[j - 1][k] = matrix[j][k];
-                    } else if (k > i) {
-                        minorMatrix[j - 1][k - 1] = matrix[j][k];
-                    }
+            
+        int ans = 0;
+        
+        // traverse all columns of 0th row
+        for(int i = 0; i < n; i++){
+            vector<vector<int>> second(n - 1, vector<int> (n - 1));
+            
+            // traverse all rows of second matrix
+            for(int j = 1; j < n; j++){
+                int x = 0;
+                
+                // traverse all columns of second matrix
+                for(int k = 0; k < n; k++){
+                    if(k == i)
+                        continue;
+                        
+                    second[j - 1][x++] = matrix[j][k];
                 }
             }
-
-            // Recursive call to calculate determinant of the minor matrix
-            int minorDet = determinantOfMatrix(minorMatrix, n - 1);
-
-            // Add the contribution of the current element to the determinant
-            det += sign * matrix[0][i] * minorDet;
+            
+            ans += matrix[0][i] * determinantOfMatrix(second, n - 1) * ((i & 1) ? -1 : 1);
         }
-
-        return det;
-    
+        
+        return ans;
     }
 };
 
