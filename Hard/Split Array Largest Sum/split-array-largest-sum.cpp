858aa4e72915c30dbd9874cf5aa4ed893a32da50
Vishal Singh <92137966+imvskalhans@users.noreply.github.com>
2024-01-07 18:07:33 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+ 
+
+
+int splitArray(int arr[], int N, int K) {
+        int low = 0;
+        int high = 0;
+        
+        for(int i = 0; i < N; i++)
+            high += arr[i], low = max(low, arr[i] - 1);
+            
+        while(low < high - 1){
+            int mid = low + (high - low) / 2;
+            
+            auto check = [&]() -> bool {
+                int cuts = 0;
+                
+                int sum = 0;
+                
+                for(int i = 0; i < N; i++){
+                    if(sum + arr[i] > mid){
+                        ++cuts;
+                        sum = arr[i];
+                    }
+                    else{
+                        sum += arr[i];
+                    }
+                }
+                
+                return cuts < K;
+            };
+            
+            if(check())
+                high = mid;
+            else
+                low = mid;
+        }
+        
+        return high;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        
+        cin>>N>>K;
+        int arr[N];
+        
+        for(int i=0 ; i<N ; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        cout<<ob.splitArray(arr,N,K);
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
